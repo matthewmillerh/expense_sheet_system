a8a94a3555730fd7f22e3248534faad145d9c964
matthewmillerh <matthewmillerh@outlook.com>
2025-07-25 15:16:13 +0200

    Initial commit

---
+++ grails-app/controllers/expense_sheet_system/UrlMappings.groovy
@@ -0,0 +1,16 @@
+package expense_sheet_system
+
+class UrlMappings {
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+
+    }
+}
---
+++ grails-app/init/expense_sheet_system/BootStrap.groovy
@@ -0,0 +1,9 @@
+package expense_sheet_system
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
