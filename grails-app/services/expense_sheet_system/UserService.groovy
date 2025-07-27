1baac1515ca88a3be266592102a49b6deeece11e
matthewmillerh <matthewmillerh@outlook.com>
2025-07-27 14:03:35 +0200

    Generated proper UserController using the CLI

---
+++ grails-app/services/expense_sheet_system/UserService.groovy
@@ -0,0 +1,18 @@
+package expense_sheet_system
+
+import grails.gorm.services.Service
+
+@Service(User)
+interface UserService {
+
+    User get(Serializable id)
+
+    List<User> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    User save(User user)
+
+}
